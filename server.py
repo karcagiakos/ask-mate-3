a736dac613144bc11b5b325666991ae31f1e2886
DevonCarswell <kaszazsolt90@gmail.com>
2022-02-23 11:22:54 +0100

    start login

--- server.py
+++ server.py
@@ -255,6 +255,12 @@ def registration():
     return render_template('registration.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+   return render_template('login.html')
+
+
 if __name__ == "__main__":
     app.run(
         #host = "192.168.1.100",
