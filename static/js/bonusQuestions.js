8cc1870baf5cbf24ca229196aad17af676a7bffb
keczanilles <illeskeczan99@gmail.com>
2022-02-24 20:01:59 +0100

    sort bonus working

--- static/js/bonusQuestions.js
+++ static/js/bonusQuestions.js
@@ -1,26 +1,30 @@
 // you receive an array of objects which you must sort in the by the key "sortField" in the "sortDirection"
 function getSortedItems(items, sortField, sortDirection) {
-    console.log(items)
-    console.log(sortField)
-    console.log(sortDirection)
-
-    // === SAMPLE CODE ===
-    // if you have not changed the original html uncomment the code below to have an idea of the
-    // effect this function has on the table
-    //
-    if (sortDirection === "asc") {
-        const firstItem = items.shift()
-        if (firstItem) {
-            items.push(firstItem)
+    if (sortDirection == "asc") {
+        function compare(a, b) {
+            if (a.Description < b.Description) {
+                return -1;
+            }
+            if (a.Description > b.Description) {
+                return 1;
+            }
+            return 0;
         }
+
+        return items.sort(compare)
     } else {
-        const lastItem = items.pop()
-        if (lastItem) {
-            items.push(lastItem)
+        function compare(a, b) {
+            if (a.Description > b.Description) {
+                return -1;
+            }
+            if (a.Description < b.Description) {
+                return 1;
+            }
+            return 0;
         }
-    }
 
-    return items
+        return items.sort(compare)
+    }
 }
 
 // you receive an array of objects which you must filter by all it's keys to have a value matching "filterValue"
